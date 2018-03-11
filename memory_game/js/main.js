e895211f63eaca25e73d59a6fc6a12adff49b5d7
Timothy <tjk315@gmail.com>
2018-03-11 18:27:22 -0400

    completed unit 11 tasks

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,17 +30,28 @@ var checkForMatch = function () {
   } else {
     alert("Sorry, try again.");
   }
-}
+};
 
-var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank);
+var flipCard = function () {
+  //console.log("User flipped " + cards[cardId].rank);
+  var cardId = this.getAttribute("data-id");
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].suit);
   console.log(cards[cardId].cardImage);
+  this.setAttribute("src", cards[cardId].cardImage )
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
-}
+};
+var createBoard = function () {
+  for (i = 0; i < cards.legnth; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board")[0].appendChild(cardElement);
+  }
+};
 
-flipCard(0);
-flipCard(2);
+
+createBoard();
